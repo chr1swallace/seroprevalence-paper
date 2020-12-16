0dd1ce2fe1b69ed61a4c2c141acbad0894d78d19
chr1swallace <cew54@cam.ac.uk>
2020-12-16 22:13:51 +0000

    update 201216 data

--- common.R
+++ common.R
@@ -14,6 +14,7 @@ file_xlsx_v2  <- file.path(d, "200608 BD + Preg All Values.xlsx")
 file_xlsx_v3  <- file.path(d, "200609 BD + Preg All Values.xlsx")
 file_xlsx_v4 <- file.path(d,"200705 BD Preg Wks 14-25.xlsx")
 file_xlsx_v5 <- file.path(d,"200708 S Re-run .xlsx")
+file_xlsx_v6 <- file.path(d,"201216_BDPW_Wk45-50.xlsx")
 
 ## patient samples
 file2_xlsx_v1 <- file.path(d,"200619 Patient Data for Chris - regression.xlsx")
@@ -25,3 +26,4 @@ file_rdata_v1 <- file.path(d,"data-200607.RData")
 file_rdata_v2 <- file.path(d,"data-200609.RData")
 file_rdata_v4 <- file.path(d,"data-200705.RData")
 file_rdata_v5 <- file.path(d,"data-200708.RData")
+file_rdata_v6 <- file.path(d,"data-201216.RData")
